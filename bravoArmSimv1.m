c1c699090fa360db70adee2bea5b34aef93cf978
hkolano <hannah.kolano@students.olin.edu>
2021-01-28 19:32:54 -0600

    started Bravo link frames

--- bravoArmSimv1.m
+++ bravoArmSimv1.m
@@ -4,20 +4,20 @@ addpath('C:\Users\hkolano\Documents\GitHub\ModernRobotics\packages\MATLAB\mr')
 
 %% Import arm setup
 bravoArm = bravoSetup();
-[b_joint_frames, Slist] = bravoKinematics();
+[b_J_frames, b_L_frames, Slist] = bravoKinematics();
 
 %% Home position
 Qspace0 = zeros(1, 7);
-Qspace1 = pi/180*[70 60 50 40 30 20 10]
+Qspace1 = pi/180*[10 60 50 40 30 20 10];
 M_home = bravoArm.fkine(Qspace0);
-T_end = FKinSpace(M_home.T, Slist, Qspace1.')
+T_end = FKinSpace(M_home.T, Slist, Qspace0.');
 
 % Qspace1 = pi/180*[0 10 0 0 0 0 0];
 % Qspace2 = pi/180*[20, 0, 20, 45, 10, 30, 10];
 
-bravoArm.teach(Qspace1, 'jointdiam', 1, 'jaxes');
+bravoArm.teach(Qspace0, 'jointdiam', 1, 'jvec');
 hold on
-trplot(T_end, 'length', 0.2, 'thick', 1, 'rviz')
+trplot(b_L_frames(8), 'length', 0.2, 'thick', 1, 'rviz')
 
 % s_jacob = bravoArm.jacob0(Qspace1)
 % b_jacob = bravoArm.jacobe(Qspace1)
--- bravoKinematics.m
+++ bravoKinematics.m
@@ -1,4 +1,4 @@
-function [bravo_joint_frames, Slist] = bravoKinematics()
+function [bravo_joint_frames, bravo_link_frames, Slist] = bravoKinematics()
 %BRAVOKINEMATICS Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -20,14 +20,55 @@ function [bravo_joint_frames, Slist] = bravoKinematics()
     [~, all] = bravoArm.fkine(QspaceStraight);
     
     Tnaught = SE3();
-    T_0g = SE3(all(1));
-    T_0f = SE3(all(2));
-    T_0e = SE3(all(3));
-    T_0d = SE3(all(4));
-    T_0c = SE3(all(5));
-    T_0b = SE3(all(6));
-    T_0a = SE3(all(7));
+    T_0g = Tnaught;
+    T_0f = SE3(all(1))
+    T_0e = SE3(all(2));
+    T_0d = SE3(all(3));
+    T_0c = SE3(all(4));
+    T_0b = SE3(all(5));
+    T_0a = SE3(all(6));
+    T_0ee = SE3(all(7));
     
     bravo_joint_frames = [Tnaught, T_0g, T_0f, T_0e, T_0d, T_0c, T_0b, T_0a];
+    
+%% ---------- HOMOGENEOUS TRANSFORMS (LINKS) ----------
+
+    R0 = rpy2r([0 0 0]);     
+    T_link1_from_jointG = SE3(R0, bravoArm.links(1).r);
+    T_0_L1 = SE3(T_0g.T*T_link1_from_jointG.T);
+    
+    % There's no way this one is right
+    T_link2_from_jointF = SE3(R0, bravoArm.links(2).r);
+    T_0_L2 = SE3(T_0f.T*T_link2_from_jointF.T);
+    
+    T_link3_from_jointE = SE3(R0, bravoArm.links(3).r);
+    T_0_L3 = SE3(T_0e.T*T_link3_from_jointE.T);
+    
+    T_link4_from_jointD = SE3(R0, bravoArm.links(4).r);
+    T_0_L4 = SE3(T_0d.T*T_link4_from_jointD.T);
+    
+    % This one is also sketchy
+    T_link5_from_jointC = SE3(R0, bravoArm.links(5).r); 
+    T_0_L5 = SE3(T_0c.T*T_link5_from_jointC.T);
+    
+    T_link6_from_jointB = SE3(R0, bravoArm.links(6).r);
+    T_0_L6 = SE3(T_0b.T*T_link6_from_jointB.T);
+    
+    T_link7_from_jointA = SE3(R0, bravoArm.links(7).r);
+    T_0_L7 = SE3(T_0a.T*T_link7_from_jointA.T);
+%     
+%     jaw_disp = 10/1000.0; % 10mm away from COM of hand
+%     
+%     T_ee_from_L5 = SE3(R0, [0, -jaw_disp, 0]);
+%     T_0_ee = SE3(T_0_L5.T*T_ee_from_L5.T);
+    bravo_link_frames = [Tnaught, T_0_L1, T_0_L2, T_0_L3, T_0_L4, T_0_L5, T_0_L6, T_0_L7];
+   
+%     num_link_frames = length(alpha_link_frames);
+%     inv_a_link_frames = [];
+%     for i = 1:num_link_frames
+%         inv_a_link_frames = [inv_a_link_frames SE3(inv(alpha_link_frames(i).T))];
+%     end  
+    
+    % inv_a_link_frames = [Tnaught, T_L1_0, T_L2_0, T_L3_0, T_L4_0, T_L5_0, T_ee_0]
 end
 
